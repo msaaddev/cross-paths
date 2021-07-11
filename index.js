ec8531fcfd70181d9ca93e3d6e87a6bd6a9937e4
msaaddev <mrsaadirfan@gmail.com>
2021-07-11 12:37:51 +0500

    👌 IMPROVE: error handling

--- index.js
+++ index.js
@@ -5,6 +5,7 @@
  * Twitter: https://twitter.com/msaaddev
  */
 
+const logSymbols = require('log-symbols');
 const nodePath = require('path');
 const { toUnix } = require('upath');
 
@@ -14,7 +15,13 @@ const { toUnix } = require('upath');
  * @param {path} - darwin path
  * @returns {path} - win32 path
  */
-const darwinToWin32 = path => nodePath.win32.normalize(path);
+const darwinToWin32 = path => {
+	if (!path) {
+		console.log(`${logSymbols.error} Please provide a valid path`);
+		return null;
+	}
+	return nodePath.win32.normalize(path);
+};
 
 /**
  *
@@ -22,7 +29,13 @@ const darwinToWin32 = path => nodePath.win32.normalize(path);
  * @param {path} - win32 path
  * @returns {path} - darwin path
  */
-const win32ToDarwin = path => toUnix(path);
+const win32ToDarwin = path => {
+	if (!path) {
+		console.log(`${logSymbols.error} Please provide a valid path`);
+		return null;
+	}
+	return toUnix(path);
+};
 
 module.exports = {
 	darwinToWin32,
